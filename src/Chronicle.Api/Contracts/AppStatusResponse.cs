c074d5fdfa83446e7ee2b92a01eccfa2e6546844
MrFr3di <ynagi-wm@mail.ru>
2026-09-19 23:43:23 +0500

    bootstrap: simplify status transport timestamp

--- src/Chronicle.Api/Contracts/AppStatusResponse.cs
+++ src/Chronicle.Api/Contracts/AppStatusResponse.cs
@@ -1,9 +1,7 @@
-using NodaTime;
-
 namespace Chronicle.Api.Contracts;
 
 public sealed record AppStatusResponse(
     string Service,
     string State,
     string Version,
-    Instant GeneratedAtUtc);
+    DateTimeOffset GeneratedAtUtc);
