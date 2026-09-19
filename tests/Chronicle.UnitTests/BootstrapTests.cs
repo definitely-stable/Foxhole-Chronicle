fb309334f204da34e453466a5bf0dbf468faeb8d
MrFr3di <ynagi-wm@mail.ru>
2026-09-20 00:19:07 +0500

    bootstrap: satisfy analyzer naming rules in unit tests

--- tests/Chronicle.UnitTests/BootstrapTests.cs
+++ tests/Chronicle.UnitTests/BootstrapTests.cs
@@ -6,13 +6,13 @@ namespace Chronicle.UnitTests;
 public sealed class BootstrapTests
 {
     [Fact]
-    public void Core_assembly_is_available()
+    public void CoreAssemblyIsAvailable()
     {
         Assert.NotNull(typeof(AssemblyMarker).Assembly);
     }
 
     [Fact]
-    public void Time_dependent_code_can_use_a_fake_clock()
+    public void TimeDependentCodeCanUseAFakeClock()
     {
         var expected = new DateTimeOffset(2026, 9, 19, 18, 30, 0, TimeSpan.Zero);
         var clock = new FakeTimeProvider(expected);
