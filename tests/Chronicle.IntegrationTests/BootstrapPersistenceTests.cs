e6ba38fb5cae2269e8072f512fe17882a346c2c0
MrFr3di <ynagi-wm@mail.ru>
2026-09-20 00:19:10 +0500

    bootstrap: satisfy analyzer naming rules in integration tests

--- tests/Chronicle.IntegrationTests/BootstrapPersistenceTests.cs
+++ tests/Chronicle.IntegrationTests/BootstrapPersistenceTests.cs
@@ -6,7 +6,7 @@ namespace Chronicle.IntegrationTests;
 public sealed class BootstrapPersistenceTests
 {
     [Fact]
-    public void DbContext_model_can_be_created_with_the_PostgreSQL_provider()
+    public void DbContextModelCanBeCreatedWithPostgreSqlProvider()
     {
         var options = new DbContextOptionsBuilder<ChronicleDbContext>()
             .UseNpgsql("Host=localhost;Database=chronicle;Username=chronicle;Password=unused")
